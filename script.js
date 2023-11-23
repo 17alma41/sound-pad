88620334d0e1a160511be265077cf1a2720a23a2
Alvaro <17alma41@gmail.com>
2023-11-23 16:35:31 +0100

    Sound-Pad with Audio

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-const pads = document.querySelectorAll(".pad")
+const pads = document.querySelectorAll(".pad");
 
-console.log(typeof pads)
+console.log(typeof pads);
 
 const audioMap = {}
-for (let i = 0; i > pads.length; i++){
+for (let i = 0; i < pads.length; i++){
     const audioPAth = pads[i].getAttribute(`data-audio`);
     const audio = new Audio(audioPAth);
-    audioMap[pads[i].innerHTML.toLowerCase()] = audio
-
+    audioMap[pads[i].innerHTML.toLowerCase()] = audio;
+    
     pads[i].addEventListener(`click`, function(){
-        console.log("pulsaste el pad", pads[i])
+        console.log("pulsaste el pad", pads[i].innerHTML);
+        const key = pads[i].innerHTML.toLocaleLowerCase();
+        //document.querySelector("#key").value = key;
+        pads[i].style.background = "red";
+        setTimeout(function(){
+            pads[i].style.background = "lightgray"
+            pads[i].style.boxshadow = "0px 5px 0px darkslateblue"
+        },100)
+        if (audio) {
+            playAudio(audio)
+        }
     })
+}
+
+function playAudio(audio){
+    audio.pause();
+    audio.currenTime = 0;
+    audio.play()
 }
