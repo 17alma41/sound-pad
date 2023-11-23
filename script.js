87bc14e47c5495687fd9f85bf0b64702e5bc017d
Alvaro <17alma41@gmail.com>
2023-11-23 15:31:15 +0100

    Save por si aca

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hola mundo");
+const pads = document.querySelectorAll(".pad")
+
+console.log(typeof pads)
+
+const audioMap = {}
+for (let i = 0; i > pads.length; i++){
+    const audioPAth = pads[i].getAttribute(`data-audio`);
+    const audio = new Audio(audioPAth);
+    audioMap[pads[i].innerHTML.toLowerCase()] = audio
+
+    pads[i].addEventListener(`click`, function(){
+        console.log("pulsaste el pad", pads[i])
+    })
+}
